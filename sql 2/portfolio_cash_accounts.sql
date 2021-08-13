a2bf7edf7a988023ef235ed41d0250e8117e9f3e
Christopher Heydolph <49565376+cheydolph@users.noreply.github.com>
2021-08-13 19:46:41 +0000

    Added modified DB

--- sql 2/portfolio_cash_accounts.sql
+++ sql 2/portfolio_cash_accounts.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `portfolio` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `portfolio`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
 -- Host: localhost    Database: portfolio
@@ -23,8 +25,9 @@ DROP TABLE IF EXISTS `cash_accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cash_accounts` (
-  `acct_number` int NOT NULL,
-  `routing_number` int NOT NULL,
+  `acct_number` varchar(12) NOT NULL,
+  `routing_number` varchar(10) DEFAULT NULL,
+  `acct_type` varchar(100) NOT NULL,
   `bank` varchar(80) NOT NULL,
   `value` float NOT NULL,
   `date_time` datetime NOT NULL,
@@ -40,6 +43,7 @@ CREATE TABLE `cash_accounts` (
 
 LOCK TABLES `cash_accounts` WRITE;
 /*!40000 ALTER TABLE `cash_accounts` DISABLE KEYS */;
+INSERT INTO `cash_accounts` VALUES ('029384736572','98726354','Savings','Citibank',21456.8,'2021-08-12 03:42:00'),('123456789876','1234567891','Checking','Citibank',2134.33,'2021-08-12 06:42:00'),('1927583671','98374653','Cash Management','Fidelity Cash',3849.23,'2021-08-12 12:42:00');
 /*!40000 ALTER TABLE `cash_accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-08-13 16:18:59
+-- Dump completed on 2021-08-13 19:36:25
