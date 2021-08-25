0fcda87679e45b958ae443ec0cd4a6f529028f0e
cheydolph <cheydolph@ufl.edu>
2021-08-25 10:51:13 -0400

    potential import fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EvaluationComponent } from './evaluation/evaluation.component';
+import { CashComponent } from './evaluation/cash/cash.component';
+import { InvestmentComponent } from './evaluation/investment/investment.component';
+import { ChartsComponent } from './charts/charts.component';
+import { NetWorthComponent } from './charts/net-worth/net-worth.component';
+import { MarketMoversComponent } from './market-movers/market-movers.component';
+import { IndicesComponent } from './market-movers/indices/indices.component';
+import { GainersComponent } from './market-movers/gainers/gainers.component';
+import { LosersComponent } from './market-movers/losers/losers.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EvaluationComponent
+    EvaluationComponent,
+    CashComponent,
+    InvestmentComponent,
+    ChartsComponent,
+    NetWorthComponent,
+    MarketMoversComponent,
+    IndicesComponent,
+    GainersComponent,
+    LosersComponent
   ],
   imports: [
     BrowserModule,
