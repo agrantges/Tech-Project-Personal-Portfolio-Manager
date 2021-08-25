6218572ef6742eb50c01955df8742e90b2140b58
fk798 <fk798@nyu.edu>
2021-08-25 10:51:19 -0400

    added changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,65 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EvaluationComponent } from './evaluation/evaluation.component';
+import { CashComponent } from './evaluation/cash/cash.component';
+import { InvestmentComponent } from './evaluation/investment/investment.component';
+import { ChartsComponent } from './charts/charts.component';
+import { NetWorthComponent } from './charts/net-worth/net-worth.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MarketMoversComponent } from './market-movers/market-movers.component';
+import { IndicesComponent } from './market-movers/indices/indices.component';
+import { GainersComponent } from './market-movers/gainers/gainers.component';
+import { LosersComponent } from './market-movers/losers/losers.component';
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EvaluationComponent
+    EvaluationComponent,
+    CashComponent,
+    InvestmentComponent,
+    ChartsComponent,
+    NetWorthComponent,
+    MarketMoversComponent,
+    IndicesComponent,
+    GainersComponent,
+    LosersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/charts/net-worth/net-worth.component.ts
+++ frontend/src/app/charts/net-worth/net-worth.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-net-worth',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NetWorthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _renderer2: Renderer2, 
+    @Inject(DOCUMENT) private _document: Document) { }
 
   ngOnInit(): void {
-  }
+    
+ }
 
 }
