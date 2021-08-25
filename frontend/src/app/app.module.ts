52f91b54d4ae3310ef0cd3b334bd764f138429b5
fk798 <fk798@nyu.edu>
2021-08-25 11:08:54 -0400

    added bootstrap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,34 +8,12 @@ import { CashComponent } from './evaluation/cash/cash.component';
 import { InvestmentComponent } from './evaluation/investment/investment.component';
 import { ChartsComponent } from './charts/charts.component';
 import { NetWorthComponent } from './charts/net-worth/net-worth.component';
-<<<<<<< HEAD
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-=======
->>>>>>> 0fcda87679e45b958ae443ec0cd4a6f529028f0e
 import { MarketMoversComponent } from './market-movers/market-movers.component';
 import { IndicesComponent } from './market-movers/indices/indices.component';
 import { GainersComponent } from './market-movers/gainers/gainers.component';
 import { LosersComponent } from './market-movers/losers/losers.component';
-<<<<<<< HEAD
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
-
->>>>>>> 0fcda87679e45b958ae443ec0cd4a6f529028f0e
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -53,23 +31,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
-    MdbAccordionModule,
-    MdbCarouselModule,
-    MdbCheckboxModule,
-    MdbCollapseModule,
-    MdbDropdownModule,
-    MdbFormsModule,
-    MdbModalModule,
-    MdbPopoverModule,
-    MdbRadioModule,
-    MdbRangeModule,
-    MdbRippleModule,
-    MdbScrollspyModule,
-    MdbTabsModule,
-    MdbTooltipModule,
-    MdbValidationModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
