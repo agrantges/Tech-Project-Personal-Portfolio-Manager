b7658600802c766ea7201fc303b1334031125278
fk798 <fk798@nyu.edu>
2021-08-26 10:50:02 -0400

    figured out line chart

--- frontend/src/app/charts/net-worth/net-worth.component.ts
+++ frontend/src/app/charts/net-worth/net-worth.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartType } from "chart.js"
-import { MultiDataSet, Label } from 'ng2-charts';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-net-worth',
@@ -9,26 +9,35 @@ import { MultiDataSet, Label } from 'ng2-charts';
 })
 export class NetWorthComponent implements OnInit {
 
-  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData: MultiDataSet = [
-    [350, 450, 100],
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
   ];
-  public doughnutChartType: ChartType = 'doughnut';
-
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: any = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType: ChartType = 'line';
+  public lineChartPlugins = [];
 
   constructor() { }
 
-  ngOnInit(): void {
-    
- }
+  ngOnInit() {
+  }
 
- // events
+ /*// events
  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
   console.log(event, active);
 }
 
 public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
   console.log(event, active);
-}
+}*/
 
 }
