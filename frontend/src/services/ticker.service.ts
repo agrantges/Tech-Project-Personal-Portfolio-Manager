d4bcaafa556e2f772f1e01ce24498383776caa8b
cheydolph <cheydolph@ufl.edu>
2021-08-27 09:18:56 -0400

    fixed gainers

--- frontend/src/services/ticker.service.ts
+++ frontend/src/services/ticker.service.ts
@@ -11,9 +11,9 @@ export class TickerService
 {
 
   private baseUrl = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?region=US&symbols="
-  private gainTickers = "AMD%2CIBM%2CJPM%2CC%2C6J%3DF"
+  private gainTickers = "DVAX%2CSNOW%2CGOGL%2C%20FLWS%2CUUP"
   private lossTickers = "JWN%2CURBN%2CAEO%2CGPS%2CARKG"
-  private indicesTickers = "%5EGSPC%2C%20%5EDJI%2C%20%5ETNX%2C%20VBMFX"
+  private indicesTickers = "%5EGSPC%2C%5EDJI%2C%5ETNX%2CNQ%3DF"
   private requestHeaders = {"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com", "x-rapidapi-key": "795ef182cfmshb983455120db60cp15230cjsnb0aae839a913"}
   
 
