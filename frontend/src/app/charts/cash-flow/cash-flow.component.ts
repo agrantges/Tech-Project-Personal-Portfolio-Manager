e53ea10883192072831bcb1c1fa7fe411b328a1b
fk798 <fk798@nyu.edu>
2021-08-27 09:43:23 -0400

    edited Cash Flow

--- frontend/src/app/charts/cash-flow/cash-flow.component.ts
+++ frontend/src/app/charts/cash-flow/cash-flow.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType } from "chart.js"
-import { MultiDataSet, Label } from 'ng2-charts';
+import { MultiDataSet, Label, Color } from 'ng2-charts';
+import { SpringServiceService } from 'src/services/spring-service.service';
 
 @Component({
   selector: 'app-cash-flow',
@@ -9,18 +10,64 @@ import { MultiDataSet, Label } from 'ng2-charts';
 })
 export class CashFlowComponent implements OnInit {
 
-  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartLabels: Label[] = [];
   public doughnutChartData: MultiDataSet = [
-    [350, 450, 100],
+    [],
   ];
   public doughnutChartType: ChartType = 'doughnut';
+  public doughnutChartColors: Color[] = [
+    {
+      backgroundColor: [
+        'rgb(139,197,241)',
+        'rgb(254,160,179)'
+      ]
+    }
+  ];
 
+  public totalCash = 0;
+  public totalInvestment = 0;
 
-  constructor() { }
+  constructor(private springService:SpringServiceService) { }
 
   ngOnInit(): void {
-    
+    this.getCash();
+    this.getInvestment();
+    console.log("doughnut")
+    console.log(this.doughnutChartData)
  }
+
+ getCash() {
+  this.springService.getCash().subscribe((data: any) => {
+    //console.log(data)
+      for (var i = 0; i < data.length; ++i) {
+        this.totalCash += data[i].value;
+      }
+      console.log(this.totalCash);
+      this.totalCash = Math.round(this.totalCash * 100) / 100;
+
+      this.doughnutChartLabels.push("Cash")
+      this.doughnutChartData[0].push(this.totalCash)
+    }, (err:any) => {
+      console.log("entry not found")
+    })
+}
+
+ getInvestment() {
+  this.springService.getInvestment().subscribe((data: any) => {
+    //console.log(data)
+      for (var i = 0; i < data.length; ++i) {
+        this.totalInvestment += data[i].value;
+      }
+      console.log(this.totalInvestment)
+      this.totalInvestment = Math.round(this.totalInvestment * 100) / 100;
+      console.log(this.totalInvestment)
+
+      this.doughnutChartLabels.push("Investment")
+      this.doughnutChartData[0].push(this.totalInvestment)
+    }, (err:any) => {
+      console.log("entry not found")
+    })
+}
 
  // events
  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
