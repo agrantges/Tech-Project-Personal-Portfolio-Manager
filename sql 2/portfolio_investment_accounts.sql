bb29a2de5c152f9dc12946cb7e3576449cd1a119
cheydolph <cheydolph@ufl.edu>
2021-08-27 14:39:54 -0400

    added snapshot

--- sql 2/portfolio_investment_accounts.sql
+++ sql 2/portfolio_investment_accounts.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `portfolio` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `portfolio`;
--- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
--- Host: localhost    Database: portfolio
+-- Host: 127.0.0.1    Database: portfolio
 -- ------------------------------------------------------
--- Server version	8.0.22
+-- Server version	8.0.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-08-19 18:05:17
+-- Dump completed on 2021-08-27 11:16:20
